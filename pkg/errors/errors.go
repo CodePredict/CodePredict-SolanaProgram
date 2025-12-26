5b69b27dd93979dd6b96a86cc125eb5b2832e9a7
maledictu <skinnyman676767@gmail.com>
2025-12-26 23:09:38 +0300

    feat: add errors handlers for solana wallet

---
+++ pkg/errors/errors.go
@@ -0,0 +1,38 @@
+package errors
+
+import "fmt"
+
+// DomainError represents a domain-level error
+type DomainError struct {
+	Code    string
+	Message string
+	Err     error
+}
+
+func (e *DomainError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s: %s (%v)", e.Code, e.Message, e.Err)
+	}
+	return fmt.Sprintf("%s: %s", e.Code, e.Message)
+}
+
+func (e *DomainError) Unwrap() error {
+	return e.Err
+}
+
+// NewDomainError creates a new domain error
+func NewDomainError(code, message string, err error) *DomainError {
+	return &DomainError{
+		Code:    code,
+		Message: message,
+		Err:     err,
+	}
+}
+
+func (e *DomainError) NotConnectWallerError() error {
+	return &DomainError{
+		Code:    "NOT_CONNECT_WALLET",
+		Message: "Not connect wallet",
+		Err:     e.Err,
+	}
+}
